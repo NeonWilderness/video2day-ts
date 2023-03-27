8575299d13d936c29651a2e0b645754a68e469eb
NeonWilderness <neonwilderness@ymail.com>
2023-03-27 15:40:06 +0200

    Clean up packages, Upgrade upload utils

--- util/updateStories.js
+++ util/updateStories.js
@@ -5,100 +5,30 @@
  * /stories/providerdocumentation (from ./dist/videoload-provider.html)
  * /stories/videotool2 (from ./dist/videotool-version2.html)
  */
-
-const cheerio = require('cheerio');
 const fs = require('fs');
-const { req, acceptTerms } = require('./_acceptTerms');
-const { loginTwoday } = require('./_login');
+const path = require('path');
+const Twoday = require('@neonwilderness/twoday');
+require('dotenv-safe').config();
 
-const blogUrl = 'https://neonwilderness.twoday.net/stories/';
-
-/**
- * Returns GET story input field values to be used in a subsequent POST
- */
-const getIncomingData = function (body, response, resolveWithFullResponse) {
-  const $ = cheerio.load(body);
-  return {
-    secretKey: $('[name="secretKey"]').val(),
-    content_title: $('[name="content_title"]').val(),
-    modNiceUrls_urlid: $('[name="modNiceUrls_urlid"]').val(),
-    addToFront: $('[name="addToFront"]').val(),
-    checkbox_addToFront: $('[name="checkbox_addToFront"]').val(),
-    addToTopic: $('[name="addToTopic"]').val(),
-    topic: $('[name="topic"]').val(),
-    editableby: $('[name="editableby"]').val(),
-    discussions: $('[name="discussions"]').val(),
-    checkbox_discussions: $('[name="checkbox_discussions"]').val(),
-    createtime: $('[name="createtime"]').val(),
-    publish: $('[name="publish"]').val()
-  };
-};
-
-/**
- * Files to be uploaded
- * name: the story's niceurl name
- * src:  the appropriate dist-file to be copied into the story
- */
 const uploadFiles = [
   { name: 'videoload2', src: 'videoload-version2.html' },
   { name: 'providerdocumentation', src: 'videoload-provider.html' },
   { name: 'videotool2', src: 'videotool-version2.html' }
 ];
 
-/**
- * Request-Promise sequence to update one file
- */
-const updateStory = (story, fileIndex) => {
-  const storyEditUrl = `${blogUrl}${story.name}/edit`;
-  console.log('Preparing to edit story:', storyEditUrl);
-  return req.get({
-    uri: storyEditUrl,
-    transform: getIncomingData
-  })
-    .then(incoming => {
-      console.log('Updating story:', storyEditUrl);
-      return req.post({
-        uri: storyEditUrl,
-        form: {
-          'secretKey': incoming.secretKey,
-          'content_title': incoming.content_title,
-          'modNiceUrls_urlid': incoming.modNiceUrls_urlid,
-          'content_text': fs.readFileSync(`${__dirname}/../dist/${story.src}`, 'utf8'),
-          'addToFront': incoming.addToFront,
-          'checkbox_addToFront': incoming.checkbox_addToFront,
-          'addToTopic': incoming.addToTopic,
-          'topic': incoming.topic,
-          'editableby': incoming.editableby,
-          'discussions': incoming.discussions,
-          'checkbox_discussions': incoming.checkbox_discussions,
-          'createtime': incoming.createtime,
-          'publish': incoming.publish
-        }
+(async () => {
+  try {
+    const td = new Twoday.Twoday('dev', { delay: 300 });
+    await td.login();
+    for (const file of uploadFiles) {
+      await td.updateStory('neonwilderness', {
+        niceurl: file.name,
+        body: fs.readFileSync(path.resolve(process.cwd(), 'dist', file.src), 'utf8')
       });
-    })
-    .then(() => {
-      console.log(`Update completed for story #${fileIndex + 1}: ${story.name} (${story.src}).\n`);
-    })
-    .catch(err => {
-      console.log('Update ***failed*** for story:', story.src, 'with Error', err);
-    });
-};
-
-const updateAllStories = async () => {
-  for (let i = 0; i < uploadFiles.length; i++) {
-    await updateStory(uploadFiles[i], i);
+      console.log(`Update completed for story: ${file.name} (${file.src}).`);
+    }
+    await td.logout();
+  } catch (err) {
+    console.log(`>>> Error updating stories: ${err}.`);
   }
-};
-
-acceptTerms()
-  .then(() => {
-    console.log('Terms accepted. Now logging in...');
-    return loginTwoday();
-  })
-  // process story/file list to be uploaded
-  .then(() => {
-    return updateAllStories();
-  })
-  .catch(err => {
-    console.log('Update failed with error: ', err);
-  });
+})();
--- util/uploadScripts.js
+++ util/uploadScripts.js
@@ -5,12 +5,9 @@
  * videoload2-tool-min-js (from ./dist/videoload2-tool.js)
  * videoload2-story       (from ./dist/videoload2-story.js)
  */
-
-const fs = require('fs');
-const { req, acceptTerms } = require('./_acceptTerms');
-const { getSecretKey, getSecretKeyAndUri, loginTwoday } = require('./_login');
-
-const fileCreateUrl = 'https://cdn.twoday.net/files/create';
+const path = require('path');
+const Twoday = require('@neonwilderness/twoday');
+require('dotenv-safe').config();
 
 /**
  * Script files to be uploaded
@@ -19,80 +16,37 @@ const fileCreateUrl = 'https://cdn.twoday.net/files/create';
  * desc: short description to be inserted
  */
 const uploadFiles = [
-  { name: 'videoload2-min-js', src: 'videoload2.js', desc: 'Videoload Version 2: ermöglicht in Twoday die Einbindung von iframe-Elementen von verschiedensten Plattformen.' },
-  { name: 'videoload2-tool-min-js', src: 'videoload2-tool.js', desc: 'Story-Script für den Beitrag stories/videotool2' },
-  { name: 'videoload2-story-min-js', src: 'videoload2-story.js', desc: 'Story-Script für den Beitrag stories/videoload2' }
+  {
+    name: 'videoload2-min-js',
+    src: 'videoload2.js',
+    desc: 'Videoload Version 2: ermöglicht in Twoday die Einbindung von iframe-Elementen von verschiedensten Plattformen.'
+  },
+  {
+    name: 'videoload2-tool-min-js',
+    src: 'videoload2-tool.js',
+    desc: 'Story-Script für den Beitrag stories/videotool2'
+  },
+  {
+    name: 'videoload2-story-min-js',
+    src: 'videoload2-story.js',
+    desc: 'Story-Script für den Beitrag stories/videoload2'
+  }
 ];
 
-/**
- * Request-Promise sequence to update one file (file needs to be deleted and re-inserted)
- */
-const updateCDNFile = (cdnFile, fileIndex) => {
-  const fileDeleteUrl = `https://cdn.twoday.net/files/${cdnFile.name}/delete`;
-  console.log('Preparing to delete file url:', fileDeleteUrl);
-  return req.get({
-    uri: fileDeleteUrl,
-    transform: getSecretKeyAndUri
-  })
-    .then(incoming => {
-      console.log('Deleting file url:', incoming.uri);
-      return req.post({
-        uri: incoming.uri,
-        form: {
-          'secretKey': incoming.secretKey,
-          'remove': 'Löschen'
-        }
+(async () => {
+  try {
+    const td = new Twoday.Twoday('dev', { delay: 300 });
+    await td.login();
+    for (const file of uploadFiles) {
+      const fileID = await td.updateFile('neonwilderness', {
+        name: file.name,
+        path: path.resolve(process.cwd(), 'dist', file.src),
+        description: file.desc
       });
-    })
-    /* Init file upload */
-    .then(() => {
-      console.log('Delete completed. Prep to recreate.');
-      return req.get({
-        uri: fileCreateUrl,
-        transform: getSecretKey
-      });
-    })
-    /* Conduct file upload */
-    .then(secretKey => {
-      return req.post({
-        uri: fileCreateUrl,
-        formData: {
-          'secretKey': secretKey,
-          'rawfile': {
-            value: fs.createReadStream(__dirname + '/../dist/' + cdnFile.src),
-            options: {
-              filename: cdnFile.src,
-              contentType: 'application/javascript'
-            }
-          },
-          'alias': cdnFile.name,
-          'description': cdnFile.desc,
-          'save': 'Sichern'
-        },
-      });
-    })
-    .then(() => {
-      console.log(`Update completed for file #${fileIndex + 1}: ${cdnFile.name} (${cdnFile.src}).\n`);
-    })
-    .catch(err => {
-      console.log('Upload ***failed*** for file:', cdnFile.src, 'with Error', err);
-    });
-};
-
-const updateAllScripts = async () => {
-  for (let i = 0; i < uploadFiles.length; i++) {
-    await updateCDNFile(uploadFiles[i], i);
+      console.log(`Update completed for file "${fileID}": ${file.name} (${file.src}).`);
+    }
+    await td.logout();
+  } catch (err) {
+    console.log(`>>> Error updating stories: ${err}.`);
   }
-};
-
-acceptTerms()
-  .then(() => {
-    console.log('Terms accepted. Now logging in...');
-    return loginTwoday();
-  })
-  .then(() => {
-    return updateAllScripts();
-  })
-  .catch(err => {
-    console.log('Upload failed with error: ', err);
-  });
+})();
